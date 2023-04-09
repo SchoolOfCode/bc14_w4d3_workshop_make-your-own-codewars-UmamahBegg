6c1ba268e7dd3117d630d549e6c392ac1dfb48c6
MariumBegg <marium.begg@hotmail.co.uk>
2023-04-09 17:33:49 +0300

    Finally done!!

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 /* 
-👉 Write your kata here!
+It's the month of Ramadan, and all your friends are trying to read the full Quran! There are 30 chapters for 30 days. You want to help them out by writing a function that will tell them how many chapters they have left to read each day.
+
+Write a function that takes in the number of chapters that have been read so far, and returns the number of chapters that are left to read.
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+export function chaptersLeft(chaptersRead) {
+    if (chaptersRead > 30) {
+        return 0;
+    } else if (chaptersRead < 0) {
+        return 30;
+    } else if (typeof chaptersRead !== 'number') {
+        return 30;
+    } else {
+        return 30 - chaptersRead;
+    }
+}
+
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,28 @@
 //👉 Write your tests below here:
+
+import { chaptersLeft } from './main.js';
+
+test ('returns the number of chapters left to read', () => {
+    expect(chaptersLeft(1)).toEqual(29);
+    expect(chaptersLeft(5)).toEqual(25);
+    expect(chaptersLeft(30)).toEqual(0);
+});
+
+test ('returns 0 if the number of chapters read is greater than 30', () => {
+    expect(chaptersLeft(31)).toEqual(0);
+    expect(chaptersLeft(100)).toEqual(0);
+});
+
+test ('returns 30 if the number of chapters read is less than 0', () => {
+    expect(chaptersLeft(-1)).toEqual(30);
+    expect(chaptersLeft(-100)).toEqual(30);
+});
+
+test ('returns 30 if the number of chapters read is not a number', () => {
+    expect(chaptersLeft('hello')).toEqual(30);
+    expect(chaptersLeft(true)).toEqual(30);
+});
+
+test ('returns 30 if the number of chapters read is not provided', () => {
+    expect(chaptersLeft()).toEqual(30);
+});
